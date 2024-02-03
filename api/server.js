dcbc30e8ae4a4ad6be5b8f4307205e0c7887dfc2
Levi Bohn <levibohn@gmail.com>
2024-02-03 13:06:36 -0600

    completed MVP alongside solution video

--- api/server.js
+++ api/server.js
@@ -4,6 +4,35 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        } else {
+                if (!req.body.name || !req.body.bio) {
+                    res.status(400).json({
+                        message: "Please provide name and bio for the user"
+                    })
+                } else {
+                    const updatedUser = await User.update(
+                        req.params.id,
+                        req.body,
+                    )
+                    res.status(200).json(updatedUser)
+                }
+            }
+        } catch (err) {
+            res.status(500).json({
+                message: 'error updating user',
+                err: err.message,
+                stack: err.stack,
+            })
+        }
+    })
+
 server.delete('/api/users/:id', async (req, res) => {
     try {
         const possibleUser = await User.findById(req.params.id)
@@ -17,7 +46,7 @@ server.delete('/api/users/:id', async (req, res) => {
         }
     } catch (err) {
         res.status(500).json({
-            message: 'error creating user',
+            message: 'error deleting user',
             err: err.message,
             stack: err.stack,
         })
@@ -30,52 +59,52 @@ server.post('/api/users/', (req, res) => {
         res.status(400).json({
             message: "Please provide name and bio for the user",
         })
-    } else{
+    } else {
         User.insert(user)
-        .then(createdUser => {
-            res.status(201).json(createdUser)
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: 'error creating user',
-                err: err.message,
-                stack: err.stack,
+            .then(createdUser => {
+                res.status(201).json(createdUser)
             })
-        })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'error creating user',
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
     }
 })
 
 server.get('/api/users', (req, res) => {
     User.find()
-    .then(users => {
-        res.json(users)
-    })
-    .catch(err => {
-        res.status(500).json({
-            message: 'error getting users',
-            err: err.message,
-            stack: err.stack,
+        .then(users => {
+            res.json(users)
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
 })
 
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
-    .then(user => {
-        if (!user) {
-            res.status(404).json({
-                message: "The user with the specified ID does not exist",
-            })
-        }
-        res.json(user)
-    })
-    .catch(err => {
-        res.status(500).json({
-            message: "error getting user",
-            err: err.message,
-            stack: err.stack,
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist",
+                })
+            }
+            res.json(user)
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                message: "error getting user",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
 })
 
 server.use('*', (req, res) => {
